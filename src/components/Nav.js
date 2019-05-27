ceedb51be6b83383ec6ade1c37700a09ca252c0c
Eric <ericgrevillius@gmail.com>
2019-05-27 11:02:40 +0200

    navbar collapse added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,9 @@ import search from '../images/1x/baseline_search_white_24dp.png'
 export function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
