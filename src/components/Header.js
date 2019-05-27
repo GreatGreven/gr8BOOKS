c15808bf8f11a6ee4c1a42e024bb4d9c3ccd325d
Eric <ericgrevillius@gmail.com>
2019-05-27 09:07:03 +0200

    Added components and Bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header className="">
+            <h1 className="display-1 text-center">GR8 Books</h1>
+        </header>
+    );
+}
