b023a2c3d3d476364cfc3d78db03e9bac673e39d
Eric <ericgrevillius@gmail.com>
2019-05-27 10:54:05 +0200

    nav bar structure done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import search from '../images/1x/baseline_search_black_24dp.png'
+import search from '../images/1x/baseline_search_white_24dp.png'
 
 export function Nav() {
     return (
-        <nav className="col-12 sticky-top">
-            <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Navigation bar">    
-                <div className="btn-group" role="group">
-                    <button className="btn btn-primary">Home</button>
-                    <button className="btn btn-primary">Favorites</button>
-                </div>
-                <div className="input-group">
-                    <input className="form-control" type="text" placeholder="Search..."></input>
-                    <div className="input-group-prepend">
-                        <button className="btn btn-primary"><img src={search} className="img-fluid" alt="search icon"></img></button>
-                    </div>
-                </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Favorites</a>
+                    </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button className="btn btn-dark my-2 my-sm-0" type="submit"><img src={search} className="img-fluid" alt="search icon"></img></button>
+                </form>
             </div>
         </nav>
     );
-}
+};
