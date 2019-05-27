d7d623c32f4156b1b6fe55decdac57d6ebc04323
Eric <ericgrevillius@gmail.com>
2019-05-27 11:07:07 +0200

    navbar collapse change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import search from '../images/1x/baseline_search_white_24dp.png'
 
 export function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
